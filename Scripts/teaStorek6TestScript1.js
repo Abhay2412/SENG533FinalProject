0ce5635789f0b55fb22fa7aa34ceb0eafbeb0252
Abhay2412 <abhaykhosla0@gmail.com>
2024-02-27 19:08:03 -0700

    Add response times to the groups

--- Scripts/teaStorek6TestScript1.js
+++ Scripts/teaStorek6TestScript1.js
@@ -2,6 +2,8 @@ import { group, sleep } from 'k6';
 
 import http from 'k6/http';
 
+const groupResponseTimes = {};
+
 export const options = {
     stages: [
         { duration: '10s', target: 5 },
@@ -19,32 +21,56 @@ export default function () {
     const BASE_URL = `http://${ABHAY_IP}:8080/tools.descartes.teastore.webui`;
 
     group('TeaStore Homepage Browse', () => {
+        const start = new Date();
         http.get(`${BASE_URL}/`);
+        const end = new Date();
+        const duration = end - start;
+        groupResponseTimes['TeaStore Homepage Browse'] = (groupResponseTimes['TeaStore Homepage Browse'] || 0) + duration;
         sleep(1);
     });
 
     group('TeaStore Login User', () => {
+        const start = new Date();
         http.get(`${BASE_URL}/login`);
-        sleep(1);
+        const end = new Date();
+        const duration = end - start;
+        groupResponseTimes['TeaStore Login User'] = (groupResponseTimes['TeaStore Login User'] || 0) + duration;
+        sleep(1);   
     });
 
     group('Black Tea Category Browse', () => {
+        const start = new Date();
         http.get(`${BASE_URL}/category?category=2&page=1`);
+        const end = new Date();
+        const duration = end - start;
+        groupResponseTimes['Black Tea Category Browse'] = (groupResponseTimes['Black Tea Category Browse'] || 0) + duration;
         sleep(1);
     });
 
     group('Earl Grey (loose) Tea Product View', () => {
+        const start = new Date();
         http.get(`${BASE_URL}/product?id=7`);
+        const end = new Date();
+        const duration = end - start;
+        groupResponseTimes['Earl Grey (loose) Tea Product View'] = (groupResponseTimes['Earl Grey (loose) Tea Product View'] || 0) + duration;
         sleep(1);
     });
 
-    group('TeaStore ViewCart', () => {
+    group('TeaStore View Cart', () => {
+        const start = new Date();
         http.get(`${BASE_URL}/cart`);
+        const end = new Date();
+        const duration = end - start;
+        groupResponseTimes['TeaStore View Cart'] = (groupResponseTimes['TeaStore View Cart'] || 0) + duration;
         sleep(1);
     });
 
     group('TeaStore Checkout', () => {
+        const start = new Date();
         http.get(`${BASE_URL}/order`);
+        const end = new Date();
+        const duration = end - start;
+        groupResponseTimes['TeaStore Checkout'] = (groupResponseTimes['TeaStore Checkout'] || 0) + duration;
         sleep(1);
     });
 }
