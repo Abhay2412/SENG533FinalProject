a810bfba8867f36e70846192b8f53b37c20fdd7a
AhadAli01 <ahadali101aa@gmail.com>
2024-03-02 17:30:43 -0700

    update process arg

---
+++ Scripts/temp.js
@@ -0,0 +1,7 @@
+const BASE_URL = `http://${process.argv[process.argv.length-1] || 'localhost'}:8080/tools.descartes.teastore.webui`;
+
+// const BASE_URL = process.argv[process.argv.length-1];
+console.log(BASE_URL);
+
+// const a = 1;
+// console.log(1);
