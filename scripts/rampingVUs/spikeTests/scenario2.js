93fb3c42c38d13991bc185c6c3713d8010fb9806
Parbir <parbir.lehal@gmail.com>
2024-03-16 15:32:42 -0600

    rampingVus

---
+++ scripts/rampingVUs/spikeTests/scenario2.js
@@ -0,0 +1,36 @@
+
+import http from 'k6/http';
+import { group, sleep } from 'k6';
+
+const BASE_URL = `http://${__ENV.HOST || 'localhost'}:8080/tools.descartes.teastore.webui`;
+
+const groupResponseTimes = {};
+
+//Spikes
+export const options = {
+  discardResponseBodies: true,
+  scenarios: {
+    blackTeaBrowse: {
+        executor: 'ramping-vus',
+        startVUs: 0,
+        stages: [
+          { duration: '30s', target: 25 },
+          { duration: '30s', target: 50 },
+          { duration: '30s', target: 75 },
+          { duration: '30s', target: 100 },
+        ],
+        gracefulRampDown: '0s',
+    },
+  },
+};
+
+export default function () {
+  group('Black Tea Category Browse', () => {
+    const start = new Date();
+    http.get(`${BASE_URL}/category?category=2&page=1`);
+    const end = new Date();
+    const duration = end - start;
+    groupResponseTimes['Black Tea Category Browse'] = (groupResponseTimes['Black Tea Category Browse'] || 0) + duration;
+    sleep(1);
+  });
+}
